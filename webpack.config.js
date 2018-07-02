190f06e0efd83c95f64b2ac816a19ec04dafaa53
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2018-07-02 20:39:54 +0900

    Hello World on IE8

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const { resolve } = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: resolve('dist')
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+    alias: {
+      'react': 'nervjs',
+      'react-dom': 'nervjs'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: 'babel-loader'
+      }
+    ]
+  }
+}
